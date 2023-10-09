86bf2681b9e1d614662da606f685654638eff2a9
david-sfernandes <62266135+david-sfernandes@users.noreply.github.com>
2023-10-08 22:05:47 -0300

    add title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import getDevices from "@/api/getDevices";
-import getDevicesByType from "@/api/getDevicesByType";
-import Card from "@/components/Card";
 import Home from "@/components/Home";
-import Info from "@/components/Info";
-import useDeviceStore from "@/store/DevicesStore";
-import Image from "next/image";
-import { useState } from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Tareabyte Monitor",
+  description: "Monitoramento de dispositivos",
+};
 
 export default async function Page() {
   return <Home />;
